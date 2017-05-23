bee84daadd833e6656f58d452975bb571ba22f39
jtrfid <jtrfid@qq.com>
2017-05-23 19:19:58 +0800

    获取强连通分量测试

--- src/edu/xidian/petrinet/test/S2PRTest.java
+++ src/edu/xidian/petrinet/test/S2PRTest.java
@@ -6,6 +6,9 @@ package edu.xidian.petrinet.test;
 import static org.junit.Assert.*;
 
 import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ArrayBlockingQueue;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -13,6 +16,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import de.invation.code.toval.validate.ParameterException;
+import de.uni.freiburg.iig.telematik.jagal.graph.exception.VertexNotFoundException;
 import de.uni.freiburg.iig.telematik.jagal.traverse.TraversalUtils;
 import de.uni.freiburg.iig.telematik.jagal.traverse.Traverser;
 import de.uni.freiburg.iig.telematik.jagal.traverse.Traverser.TraversalMode;
@@ -80,7 +85,7 @@ public class S2PRTest {
 		s2pr.isS2P();
 	}
 	
-	@Test
+	//@Test
 	public void testTraverser1() {
 		PTNet ptnet1 = new PTNet();
 		ptnet1.addPlace("p1");
@@ -163,7 +168,7 @@ public class S2PRTest {
 	}
 	
 	//@Test
-	public void testTraverser3() {
+	public void isStronglyConnected() {
 		PTNet ptnet1 = new PTNet();
 		ptnet1.addPlace("p1");
 		ptnet1.addTransition("t1");
@@ -182,7 +187,7 @@ public class S2PRTest {
 		ptnet2.removeTransition("t1");
 		System.out.println("ptnet2: " + ptnet2);
 		
-		// p1可以到图的任何点
+		// p1可以到图的其它任何点，但是不检查是否封闭
 		System.out.println("强连通：" + TraversalUtils.isStronglyConnected(ptnet1, ptnet1.getPlace("p1")));
 		
 		// p1不能到图的所有节点
@@ -195,5 +200,265 @@ public class S2PRTest {
 		System.out.println("两个网的p1名义相同，equals是 " + ptnet1.getPlace("p1").equals(ptnet2.getPlace("p1")));
 	}
 	
+	// 获取强连通分量
+	//@Test
+	public void getStronglyConnectedComponents1() {
+		PTNet ptnet1 = new PTNet();
+		ptnet1.addPlace("p1");
+		ptnet1.addTransition("t1");
+		ptnet1.addPlace("p2");
+		ptnet1.addTransition("t2");
+		ptnet1.addFlowRelationPT("p1", "t1");
+		ptnet1.addFlowRelationTP("t1", "p2");
+		ptnet1.addFlowRelationPT("p2", "t2");
+		ptnet1.addFlowRelationTP("t2", "p1");
+
+		System.out.println("ptnet1：" + ptnet1);
+		
+		Set<Set<AbstractPNNode<PTFlowRelation>>> Components;
+		Components = TraversalUtils.getStronglyConnectedComponents(ptnet1);
+		
+		System.out.println("Components: " + Components);
+		System.out.println("size=" + Components.size());
+		System.out.println("isEmpty=" + Components.isEmpty());
+		
+		Iterator<Set<AbstractPNNode<PTFlowRelation>>> iter1 = Components.iterator(); 
+		while (iter1.hasNext()) {
+		  Set<AbstractPNNode<PTFlowRelation>> nodes = iter1.next();
+		  System.out.println("contains p1 = " + nodes.contains(ptnet1.getPlace("p1")));
+		  Iterator<AbstractPNNode<PTFlowRelation>> iter2 = nodes.iterator();
+		  while(iter2.hasNext()) {
+			  AbstractPNNode<PTFlowRelation> node = iter2.next();
+			  System.out.print(node + ",");
+		  }
+		  System.out.println("");
+		}  
+	}
 	
+	// 获取强连通分量, S2P(simple sequential process)，一个包含全部节点的连通分量，这里是1个含p1的回路
+	// @Test
+	public void getStronglyConnectedComponents2() {
+		PTNet ptnet1 = new PTNet();
+		ptnet1.addPlace("p1");
+		ptnet1.addTransition("t1");
+		ptnet1.addPlace("p2");
+		ptnet1.addTransition("t2");
+		ptnet1.addPlace("p3");
+		ptnet1.addTransition("t3");
+		
+		ptnet1.addFlowRelationPT("p1", "t1");
+		ptnet1.addFlowRelationTP("t1", "p2");
+		ptnet1.addFlowRelationPT("p2", "t2");
+		ptnet1.addFlowRelationTP("t2", "p3");
+		ptnet1.addFlowRelationPT("p3", "t3");
+		ptnet1.addFlowRelationTP("t3", "p1");
+
+		System.out.println("ptnet1：" + ptnet1);
+		
+		Set<Set<AbstractPNNode<PTFlowRelation>>> Components;
+		Components = TraversalUtils.getStronglyConnectedComponents(ptnet1);
+		
+		System.out.println("Components: " + Components); // [[t3[t3], t2[t2], p1[p1], t1[t1], p3[p3], p2[p2]]]
+		System.out.println("size=" + Components.size()); // 1
+		System.out.println("isEmpty=" + Components.isEmpty()); // false
+		
+		Iterator<Set<AbstractPNNode<PTFlowRelation>>> iter1 = Components.iterator(); 
+		while (iter1.hasNext()) {
+		  Set<AbstractPNNode<PTFlowRelation>> nodes = iter1.next();
+		  System.out.println("contains p1 = " + nodes.contains(ptnet1.getPlace("p1"))); // true
+		  Iterator<AbstractPNNode<PTFlowRelation>> iter2 = nodes.iterator();
+		  while(iter2.hasNext()) {
+			  AbstractPNNode<PTFlowRelation> node = iter2.next();
+			  System.out.print(node + ","); // t3[t3],t2[t2],p1[p1],t1[t1],p3[p3],p2[p2],
+		  }
+		  System.out.println("");
+		}  
+		
+		// 结论，一个包含全部节点的连通分量
+		boolean ok = false;
+		if (Components.size() == 1) {
+		   Set<AbstractPNNode<PTFlowRelation>> nodes = Components.iterator().next();
+		   ok = nodes.size() == ptnet1.nodeCount(); 
+		}
+		System.out.println("结论，一个包含全部节点的连通分量：" + ok);
+	}
+	
+	// 获取强连通分量, S2P(simple sequential process)，一个包含全部节点的连通分量，这里是2个含p1的回路
+	//@Test
+	public void getStronglyConnectedComponents3() {
+		PTNet ptnet1 = new PTNet();
+		ptnet1.addPlace("p1");
+		ptnet1.addTransition("t1");
+		ptnet1.addPlace("p2");
+		ptnet1.addTransition("t2");
+		ptnet1.addPlace("p3");
+		ptnet1.addTransition("t3");
+		
+		ptnet1.addFlowRelationPT("p1", "t1");
+		ptnet1.addFlowRelationTP("t1", "p2");
+		ptnet1.addFlowRelationPT("p2", "t2");
+		ptnet1.addFlowRelationTP("t2", "p3");
+		ptnet1.addFlowRelationPT("p3", "t3");
+		ptnet1.addFlowRelationTP("t3", "p1");
+		
+		ptnet1.addTransition("tt1");
+		ptnet1.addPlace("pp2");
+		ptnet1.addTransition("tt2");
+		ptnet1.addPlace("pp3");
+		ptnet1.addTransition("tt3");
+		
+		ptnet1.addFlowRelationPT("p1", "tt1");
+		ptnet1.addFlowRelationTP("tt1", "pp2");
+		ptnet1.addFlowRelationPT("pp2", "tt2");
+		ptnet1.addFlowRelationTP("tt2", "pp3");
+		ptnet1.addFlowRelationPT("pp3", "tt3");
+		ptnet1.addFlowRelationTP("tt3", "p1");
+
+		System.out.println("ptnet1：" + ptnet1);
+		
+		Set<Set<AbstractPNNode<PTFlowRelation>>> Components;
+		Components = TraversalUtils.getStronglyConnectedComponents(ptnet1);
+		
+		System.out.println("Components: " + Components); // [[t3[t3], t2[t2], p1[p1], t1[t1], p3[p3], p2[p2]]]
+		System.out.println("size=" + Components.size()); // 1
+		System.out.println("isEmpty=" + Components.isEmpty()); // false
+		
+		Iterator<Set<AbstractPNNode<PTFlowRelation>>> iter1 = Components.iterator(); 
+		while (iter1.hasNext()) {
+		  Set<AbstractPNNode<PTFlowRelation>> nodes = iter1.next();
+		  System.out.println("contains p1 = " + nodes.contains(ptnet1.getPlace("p1"))); // true
+		  Iterator<AbstractPNNode<PTFlowRelation>> iter2 = nodes.iterator();
+		  while(iter2.hasNext()) {
+			  AbstractPNNode<PTFlowRelation> node = iter2.next();
+			  System.out.print(node + ","); // t3[t3],t2[t2],p1[p1],t1[t1],p3[p3],p2[p2],
+		  }
+		  System.out.println("");
+		} 
+		
+		// 结论，一个包含全部节点的连通分量
+		boolean ok = false;
+		if (Components.size() == 1) {
+		   Set<AbstractPNNode<PTFlowRelation>> nodes = Components.iterator().next();
+		   ok = nodes.size() == ptnet1.nodeCount(); 
+		}
+		System.out.println("结论，一个包含全部节点的连通分量：" + ok);
+		
+		// p1到p1没有路径
+		AbstractPNNode<PTFlowRelation> p1 = ptnet1.getPlace("p1");
+		AbstractPNNode<PTFlowRelation> p2 = ptnet1.getPlace("p2");
+		try {
+			ArrayBlockingQueue<List<AbstractPNNode<PTFlowRelation>>> paths = TraversalUtils.getDirectedPathsFor(ptnet1,p1,p2);
+			System.out.println("paths:"+paths);
+		} catch (ParameterException e) {
+			ok = false;
+			e.printStackTrace();
+		} catch (VertexNotFoundException e) {
+			ok = false;
+			e.printStackTrace();
+		}
+		
+		// 为何没有Cycle？如果各节点双向连接，是Cycle
+		System.out.println("hasCycle:" + TraversalUtils.hasCycle(ptnet1));
+	}
+	
+	// 如果各节点双向连接，是Cycle
+	@Test
+	public void hasCycle1() {
+		PTNet ptnet1 = new PTNet();
+		ptnet1.addPlace("p1");
+		ptnet1.addTransition("t1");
+		ptnet1.addPlace("p2");
+		ptnet1.addTransition("t2");
+		ptnet1.addFlowRelationPT("p1", "t1");
+		ptnet1.addFlowRelationTP("t1", "p2");
+		ptnet1.addFlowRelationPT("p2", "t2");
+		ptnet1.addFlowRelationTP("t2", "p1");
+		
+
+		System.out.println("ptnet1：" + ptnet1);
+		
+		// 为何没有Cycle？如果各节点双向连接，是Cycle
+		System.out.println("hasCycle:" + TraversalUtils.hasCycle(ptnet1));
+		
+		try {
+			System.out.println("p1是t1的前继：" + TraversalUtils.isPredecessor(ptnet1, ptnet1.getPlace("p1"), ptnet1.getTransition("t1")));
+		} catch (ParameterException | VertexNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			System.out.println("p1是t2的前继：" + TraversalUtils.isPredecessor(ptnet1, ptnet1.getPlace("p1"), ptnet1.getTransition("t2")));
+		} catch (ParameterException | VertexNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			System.out.println("p1是t1的后继：" + TraversalUtils.isSuccessor(ptnet1, ptnet1.getPlace("p1"), ptnet1.getTransition("t1")));
+		} catch (ParameterException | VertexNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			System.out.println("p1是t2的后继：" + TraversalUtils.isSuccessor(ptnet1, ptnet1.getPlace("p1"), ptnet1.getTransition("t2")));
+		} catch (ParameterException | VertexNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	// 如果各节点双向连接，是Cycle
+	//@Test
+	public void hasCycle2() {
+		PTNet ptnet1 = new PTNet();
+		ptnet1.addPlace("p1");
+		ptnet1.addTransition("t1");
+		ptnet1.addPlace("p2");
+		ptnet1.addTransition("t2");
+		ptnet1.addFlowRelationPT("p1", "t1");
+		ptnet1.addFlowRelationTP("t1", "p2");
+		ptnet1.addFlowRelationPT("p2", "t2");
+		ptnet1.addFlowRelationTP("t2", "p1");
+		
+		ptnet1.addPlace("p3");
+		ptnet1.addFlowRelationPT("p3", "t1");
+		ptnet1.addFlowRelationTP("t2", "p3");
+		
+		System.out.println("ptnet1：" + ptnet1);
+		
+		// 为何没有Cycle？
+		System.out.println("hasCycle:" + TraversalUtils.hasCycle(ptnet1));
+		
+		
+		try {
+			System.out.println("p1是t1的前继：" + TraversalUtils.isPredecessor(ptnet1, ptnet1.getPlace("p1"), ptnet1.getTransition("t1")));
+		} catch (ParameterException | VertexNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			System.out.println("p1是t2的前继：" + TraversalUtils.isPredecessor(ptnet1, ptnet1.getPlace("p1"), ptnet1.getTransition("t2")));
+		} catch (ParameterException | VertexNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			System.out.println("p1是t1的后继：" + TraversalUtils.isSuccessor(ptnet1, ptnet1.getPlace("p1"), ptnet1.getTransition("t1")));
+		} catch (ParameterException | VertexNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			System.out.println("p1是t2的后继：" + TraversalUtils.isSuccessor(ptnet1, ptnet1.getPlace("p1"), ptnet1.getTransition("t2")));
+		} catch (ParameterException | VertexNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
