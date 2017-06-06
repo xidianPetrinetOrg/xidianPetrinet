199d5e9bb0411196d9aef62421883929507b34bb
jtrfid <jtrfid@qq.com>
2017-06-07 00:25:57 +0800

    add(S2PR);  H(r)

--- src/edu/xidian/petrinet/S3PR.java
+++ src/edu/xidian/petrinet/S3PR.java
@@ -3,10 +3,17 @@
  */
 package edu.xidian.petrinet;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+import de.uni.freiburg.iig.telematik.sepia.petrinet.abstr.AbstractPNNode;
+import de.uni.freiburg.iig.telematik.sepia.petrinet.pt.PTFlowRelation;
 import de.uni.freiburg.iig.telematik.sepia.petrinet.pt.PTNet;
+import de.uni.freiburg.iig.telematik.sepia.petrinet.pt.PTPlace;
+import de.uni.freiburg.iig.telematik.sepia.petrinet.pt.PTTransition;
 
 /**
  * <pre>
@@ -18,7 +25,7 @@ import de.uni.freiburg.iig.telematik.sepia.petrinet.pt.PTNet;
  * 1. 一个S2PR是一个S3PR;
  * 2. 令Ni = (PAi ∪ {p0i} ∪ PRi , Ti, Fi), i ∈  {1,2}是两个S3PR, 使得
  *    (PA1 ∪ {p01}) ∩ (PA2 ∪ {p02}) = ∅,
- *    PR1 ∩ PR2 = PC ≠ ∅ ;, T1 ∩ T2 = ∅;
+ *    PR1 ∩ PR2 = PC ≠ ∅; T1 ∩ T2 = ∅;
  *    那么, 由N1和N2通过PC复合而成的Petri网N = (PA ∪ P0 ∪ PR, T, F)仍是S3PR, 定义为: 
  *    (1) PA = PA1 ∪ PA2; (2) P0 = {p01} ∪ {p02}; (3) PR = PR1 ∪  PR2; (4) T = T1 ∪ T2, F = F1 ∪  F2。
  * (P0)后置集中的变迁称为源变迁, 它们表示工件进入系统的输入点。(P0)前置集中的变迁称为汇变迁, 它们表示工件移出系统的输出点。
@@ -34,6 +41,11 @@ public class S3PR extends S2PR {
 	 * 组成S3PR的各个S2PR对象
 	 */
 	protected final Collection<S2PR> s2prSet = new HashSet<>();
+	
+	/**
+     * 闲置库所集合
+     */
+    protected final List<PTPlace>  P0 = new ArrayList<>();
 
 	/**
 	 * 
@@ -43,22 +55,130 @@ public class S3PR extends S2PR {
 	}
 	
 	/**
-	 * 有一个S2PR对象构造S3PR对象
+	 * 由一个S2PR对象构造S3PR对象
 	 * @param s2pr
 	 */
 	public S3PR(S2PR s2pr) {
 		super(s2pr, s2pr.p0.getName(),s2pr.getPA(),s2pr.getPR());
+		s2prSet.add(s2pr);
+		P0.add(s2pr.p0);
+	}
+	
+	/**
+	 * 由一个添加符合S2PR定义的Petri网对象构造S3PR对象
+	 * @param ptnet 符合S2PR定义的Petri网对象
+	 * @param p0   闲置库所名称
+	 * @param PA 工序库所名称集合
+	 * @param PR 资源库所名称集合
+	 */
+	public S3PR(PTNet ptnet, String p0, Collection<String> PA, Collection<String> PR) {
+		S2PR s2pr = new S2PR(ptnet,p0,PA,PR);
+		this.add(s2pr);
+	}
+	
+	/**
+	 * 添加S2PR对象到本S3PR对象中
+	 * @param s2pr
+	 */
+	public void add(S2PR s2pr) {
+		// 把s2pr的闲置库所p0加入到本对象
+		this.addPlace(s2pr.p0.getName());
+		for (PTPlace p: s2pr.PA) {
+			this.addPlace(p.getName());
+		}
+		
+		// 共享资源库所，已经在本对象中，因此不用添加
+		List<PTPlace> PC = new ArrayList<>();
+		PC.addAll(this.PR);
+		PC.retainAll(s2pr.PR);
+		
+		// s2pr.PR与PC的差集，即s2pr.PR \ PC， 将其加入到本对象中
+		List<PTPlace> PC1 = new ArrayList<>();
+		PC1.addAll(s2pr.PR);
+		PC1.removeAll(PC);
+		for (PTPlace p: PC1) {
+			this.addPlace(p.getName());
+		}
+		
+		// T = T1 ∪ T2, 把s2pr的Transitions加入到本对象
+		for (PTTransition t: s2pr.getTransitions()) {
+			this.addTransition(t.getName());
+		}
+		
+		// F = F1 ∪  F2, 把s2pr的FlowRelations加入到本对象
+		for (PTFlowRelation f : s2pr.getFlowRelations()) {
+        	if (f.getDirectionPT()) {
+        		this.addFlowRelationPT(f.getPlace().getName(), f.getTransition().getName());
+        	}
+        	else {
+            	this.addFlowRelationTP(f.getTransition().getName(), f.getPlace().getName());
+        	}
+        }
+		
+		// PA = PA1 ∪ PA2;
+		PA.addAll(s2pr.PA); // 不会有重复元素，(PA1 ∪ {p01}) ∩ (PA2 ∪ {p02}) = ∅,
+		
+		// PR = PR1 ∪  PR2;
+		// PR.addAll(s2pr.PR); // 因为有共享资源库所PC，导致重复元素。PR1 ∩ PR2 = PC ≠ ∅;
+		for (PTPlace p: s2pr.PR) {  // 保证没有重复元素
+			if (!PR.contains(p)) PR.add(p);
+		}
+		
+		// P0 = {p01} ∪ {p02};
+		P0.add(s2pr.p0);    // 不会有重复元素，(PA1 ∪ {p01}) ∩ (PA2 ∪ {p02}) = ∅,
 		s2prSet.add(s2pr);	
 	}
+	
+	/**
+	 * 添加符合S2PR定义的Petri网对象到本S3PR对象中
+	 * @param ptnet 符合S2PR定义的Petri网对象
+	 * @param p0   闲置库所名称
+	 * @param PA 工序库所名称集合
+	 * @param PR 资源库所名称集合
+	 */
+	public void add(PTNet ptnet, String p0, Collection<String> PA, Collection<String> PR) {
+		S2PR s2pr = new S2PR(ptnet,p0,PA,PR);
+		add(s2pr);
+	}
+	
+	/**
+	 * r∈PR, 使用r的工序库所称为r的持有者, 集合H(r) = ((r的前置集的前置集) ∩ PA) 称为r的持有者的集合.
+     * @param r 资源库所
+	 * @return r的持有者集合
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public Set<PTPlace> getHr(PTPlace r) {
+		Set<PTPlace> Hr = new HashSet<>();
+		Set<AbstractPNNode<PTFlowRelation>> nodes = r.getParents();
+		for (AbstractPNNode node: nodes) {
+			Hr.addAll(node.getParents());
+		}
+		Hr.retainAll(PA);
+		return Hr;
+	}
 
+	/**
+	 * 令r∈PR是S3PR网N = (P0  ∪ PA ∪ PR,T,F)的资源库所, S是N的严格极小信标。使用r的工序库所称为r的持有者, 集合H(r) = ((r的前置集的前置集) ∩ PA) 称为r的持有者的集合.
+	 * S = S<sub>R</sub> ∪ S<sub>A</sub>, 其中S<sub>R</sub>表示S中的资源库所集合，且|S<sub>R</sub>|≥2, S<sub>A</sub>表示S中的工序库所集合.
+	 * @return S3PR网所有资源的持有者集合， ∪<sub>r∈S<sub>R</sub></sub>(H(r))
+	 */
+	public Set<PTPlace> getHr(Set<PTPlace> SR) {
+		Set<PTPlace> Hrs = new HashSet<>();
+		for (PTPlace r: SR) {
+			Hrs.addAll(getHr(r));
+		}
+		return Hrs;
+	}
+	
 	@Override
 	public String toString(){
 		StringBuilder str = new StringBuilder();
 	    String superString = super.toString();
-	    //str.append("p0: " + p0.toString() + "\n"); 
-	    str.append("p0: " + p0 + "\n");  // 等效
+	    str.append("S3PR --------" + "\n");
+	    str.append("P0: " + P0 + "\n"); 
 	    str.append("PA: " + PA.toString() + "\n");
 	    str.append("PR: " + PR.toString() + "\n");
 	    return String.format("%s%n%s", superString, str);
 	}
 }
+ 
--- src/edu/xidian/petrinet/test/S3PRTest.java
+++ src/edu/xidian/petrinet/test/S3PRTest.java
@@ -4,7 +4,6 @@
 package edu.xidian.petrinet.test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -16,10 +15,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import de.uni.freiburg.iig.telematik.sepia.petrinet.abstr.AbstractPNNode;
-import de.uni.freiburg.iig.telematik.sepia.petrinet.pt.PTMarking;
 import de.uni.freiburg.iig.telematik.sepia.petrinet.pt.PTNet;
-import edu.xidian.petrinet.PetriNetTraversalUtils;
+import de.uni.freiburg.iig.telematik.sepia.petrinet.pt.PTPlace;
 import edu.xidian.petrinet.S2PR;
 import edu.xidian.petrinet.S3PR;
 
@@ -60,7 +57,7 @@ public class S3PRTest {
 	/**
 	 * Test method for {@link edu.xidian.petrinet.S3PR#S3PR(S2PR)}.
 	 */
-	@Test
+	//@Test
 	public void testS3PR() {
 		//fail("Not yet implemented");
 		S2PR s2pr = new S2PR(2,1,4);
@@ -68,6 +65,158 @@ public class S3PRTest {
 		
 		S3PR s3pr = new S3PR(s2pr);
 		System.out.println("s3pr: " + s3pr);
+	}
+	
+	/**
+	 * Test method for {@link edu.xidian.petrinet.S3PR#S3PR(PTNet, String, Collection, Collection)}.
+	 */
+	//@Test
+	public void testS3PR1() {
+		///////////////////// ptnet1
+		PTNet ptnet1 = new PTNet();
+		ptnet1.addPlace("p1");
+		ptnet1.addTransition("t1");
+		ptnet1.addPlace("p2");
+		ptnet1.addTransition("t2");
+		ptnet1.addPlace("p3");
+		ptnet1.addTransition("t3");
+		ptnet1.addPlace("p7");
+		ptnet1.addPlace("p8");
+		
+		// p0,pa
+		ptnet1.addFlowRelationPT("p1", "t1");
+		ptnet1.addFlowRelationTP("t1", "p2");
+		ptnet1.addFlowRelationPT("p2", "t2");
+		ptnet1.addFlowRelationTP("t2", "p3");
+		ptnet1.addFlowRelationPT("p3", "t3");
+		ptnet1.addFlowRelationTP("t3", "p1");
+		
+		// pr
+		ptnet1.addFlowRelationPT("p7", "t1");
+		ptnet1.addFlowRelationTP("t2", "p7");
+		ptnet1.addFlowRelationPT("p8", "t2");
+		ptnet1.addFlowRelationTP("t3", "p8");
+		
+		Set<String> pa = new HashSet<String>();
+		pa.add("p2"); pa.add("p3");
+		Set<String> pr = new HashSet<String>();
+		pr.add("p7"); pr.add("p8");
+		
+		///////////////////// ptnet2
+		PTNet ptnet2 = new PTNet();
+		ptnet2.addPlace("p4");
+		ptnet2.addTransition("t4");
+		ptnet2.addPlace("p5");
+		ptnet2.addTransition("t5");
+		ptnet2.addPlace("p6");
+		ptnet2.addTransition("t6");
+		ptnet2.addPlace("p7");
+		ptnet2.addPlace("p8");
+		
+		// p0,pa
+		ptnet2.addFlowRelationPT("p6", "t4");
+		ptnet2.addFlowRelationTP("t4", "p4");
+		ptnet2.addFlowRelationPT("p4", "t5");
+		ptnet2.addFlowRelationTP("t5", "p5");
+		ptnet2.addFlowRelationPT("p5", "t6");
+		ptnet2.addFlowRelationTP("t6", "p6");
+		
+		// pr
+		ptnet2.addFlowRelationPT("p7", "t5");
+		ptnet2.addFlowRelationTP("t6", "p7");
+		ptnet2.addFlowRelationPT("p8", "t4");
+		ptnet2.addFlowRelationTP("t5", "p8");
+		
+		S3PR s3pr = new S3PR(ptnet1,"p1",pa,pr);
+		System.out.println("s3pr: " + s3pr);
+		
+		pa.clear();
+		pa.add("p4"); pa.add("p5");
+		pr.clear();
+		pr.add("p7"); pr.add("p8");
+		s3pr.add(ptnet2,"p6",pa,pr);
+		System.out.println("s3pr: " + s3pr);
+	}
+	
+	/**
+	 * Test method for {@link edu.xidian.petrinet.S3PR#getHr(Set)}.
+	 */
+	@Test
+	public void getHrs() {
+		///////////////////// ptnet1
+		PTNet ptnet1 = new PTNet();
+		ptnet1.addPlace("p1");
+		ptnet1.addTransition("t1");
+		ptnet1.addPlace("p2");
+		ptnet1.addTransition("t2");
+		ptnet1.addPlace("p3");
+		ptnet1.addTransition("t3");
+		ptnet1.addPlace("p7");
+		ptnet1.addPlace("p8");
+		
+		// p0,pa
+		ptnet1.addFlowRelationPT("p1", "t1");
+		ptnet1.addFlowRelationTP("t1", "p2");
+		ptnet1.addFlowRelationPT("p2", "t2");
+		ptnet1.addFlowRelationTP("t2", "p3");
+		ptnet1.addFlowRelationPT("p3", "t3");
+		ptnet1.addFlowRelationTP("t3", "p1");
+		
+		// pr
+		ptnet1.addFlowRelationPT("p7", "t1");
+		ptnet1.addFlowRelationTP("t2", "p7");
+		ptnet1.addFlowRelationPT("p8", "t2");
+		ptnet1.addFlowRelationTP("t3", "p8");
+		
+		Set<String> pa = new HashSet<String>();
+		pa.add("p2"); pa.add("p3");
+		Set<String> pr = new HashSet<String>();
+		pr.add("p7"); pr.add("p8");
+		
+		///////////////////// ptnet2
+		PTNet ptnet2 = new PTNet();
+		ptnet2.addPlace("p4");
+		ptnet2.addTransition("t4");
+		ptnet2.addPlace("p5");
+		ptnet2.addTransition("t5");
+		ptnet2.addPlace("p6");
+		ptnet2.addTransition("t6");
+		ptnet2.addPlace("p7");
+		ptnet2.addPlace("p8");
+		
+		// p0,pa
+		ptnet2.addFlowRelationPT("p6", "t4");
+		ptnet2.addFlowRelationTP("t4", "p4");
+		ptnet2.addFlowRelationPT("p4", "t5");
+		ptnet2.addFlowRelationTP("t5", "p5");
+		ptnet2.addFlowRelationPT("p5", "t6");
+		ptnet2.addFlowRelationTP("t6", "p6");
+		
+		// pr
+		ptnet2.addFlowRelationPT("p7", "t5");
+		ptnet2.addFlowRelationTP("t6", "p7");
+		ptnet2.addFlowRelationPT("p8", "t4");
+		ptnet2.addFlowRelationTP("t5", "p8");
+		
+		S3PR s3pr = new S3PR(ptnet1,"p1",pa,pr);
+		
+		pa.clear();
+		pa.add("p4"); pa.add("p5");
+		pr.clear();
+		pr.add("p7"); pr.add("p8");
+		s3pr.add(ptnet2,"p6",pa,pr);
+		System.out.println("s3pr: " + s3pr);
+		
+		//////////////////////////////////////////////////
+		Set<PTPlace> SR = new HashSet<>();
+		SR.add(s3pr.getPlace("p7"));
+		SR.add(s3pr.getPlace("p8"));
+		Set<PTPlace> Hrs = s3pr.getHr(SR);
+		System.out.println("Hrs: " + Hrs); // Hrs: [p4[p4], p5[p5], p3[p3], p2[p2]]
+		assertEquals(4, Hrs.size());
+		
+		System.out.println("H(p7) = " + s3pr.getHr(s3pr.getPlace("p7"))); // H(p7) = [p5[p5], p2[p2]]
+		System.out.println("H(p8) = " + s3pr.getHr(s3pr.getPlace("p8"))); // H(p8) = [p4[p4], p3[p3]]
 	}
 	
 }
